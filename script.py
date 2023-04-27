2ef4c359f5a9138db771c1d729dc212e0ef44f3e
Stoyan Georgiev <stoyandg@yahoo.com>
2023-04-27 10:24:47 +0300

    add sys.argv for folder path, file type and desired slicing

--- script.py
+++ script.py
@@ -6,7 +6,8 @@ os.chdir((sys.argv[1]))
 for file in glob.glob((sys.argv[2])):
     file_name = os.path.splitext(file)[0]
     extension = os.path.splitext(file)[1]
-    new_file_name = file_name[:-6] + extension
+    characters_to_remove = int(sys.argv[3])
+    new_file_name = file_name[:-characters_to_remove] + extension
     try:
         os.rename(file, new_file_name)
     except OSError as e:
